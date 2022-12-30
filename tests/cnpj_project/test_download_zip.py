fcdd4e4148e20803e01c0031eb0dd6ae9c298912
Crystian0704 <crystian.alexsander@gmail.com>
2022-12-30 00:47:49 -0300

    Adicionado extensão .zip no .gitignore e criado test de url

---
+++ tests/cnpj_project/test_download_zip.py
@@ -0,0 +1,16 @@
+# import download_zip.py from cnpj_project
+# import pytest
+
+import pytest
+from cnpj_project.download_zip import get_data, download_zip
+from pathlib import Path
+
+def test_get_data():
+    #assert list is not empty
+    assert get_data() != [], "List is empty"
+    #assert list is url
+    assert get_data()[0].startswith('http'), "List is not url"
+    #assert list end with zip
+    assert get_data()[0].endswith('.zip'), "List is not zip"
+
+
